eda8173269cfc4dd72d254d2b022424ed64a8d6f
David Terry <me@xwvvvvwx.com>
2018-09-08 14:02:12 +0200

    add frame to closeWidget

--- src/components/CloseWidget.jsx
+++ src/components/CloseWidget.jsx
@@ -3,9 +3,11 @@ import React, { Component } from 'react';
 class CloseWidget extends Component {
   render() {
     return (
+    <section className="frame">
       <div style={ {position: 'relative'} }>
       HI
       </div>
+    </section>
     )
   }
 }
